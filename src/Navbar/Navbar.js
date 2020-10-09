d94f118822fa2bc4f02503badabcdbba6b65146a
Your Name <henrikh111@outlook.com>
2020-10-09 12:24:43 +0400

    Navbar

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from './Navbar.css';
+export default function Navbar() {
+    return (
+        <div className="Navbar">
+            <button>Dashboard</button>
+            <button>Faculties</button>
+            <button>Groups</button>
+            <button>Students</button>
+        </div>
+    )
+}
