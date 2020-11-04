aa3a46d3efcce015b7e059b07beec35369d97d3a
Your Name <henrikh111@outlook.com>
2020-11-04 15:57:39 +0400

    onClickHandler

--- src/Faculties/Faculties.js
+++ src/Faculties/Faculties.js
@@ -1,9 +1,22 @@
 import React,{ useState }  from 'react';
+
 import Fac from './Faculties.css'
 export default function Faculties() {
     const [toggle, setToggle] = useState(true);
   
-   
+  
+let value=null;
+let arr=[];
+  const onInputHandler=(e)=>{
+      value=e.target.value;
+    console.log(value)
+}
+const onClickHandler=()=>{
+    
+    arr.push(value)
+    setToggle(!toggle)
+    console.log(arr)
+    }
     return (
        <>
        {toggle ?  <div className="container">
@@ -16,16 +29,20 @@ export default function Faculties() {
             <div className="nameId">
             <h1>Id</h1>
             <h1 className="name">Name</h1>
+     
             </div>
+          <ul>
+          {arr.map((el)=><li>{el+10}</li>)}
+        </ul>  
         </div> : <div className="modalcontainer">
             <h1>Create Faculty</h1>
             
                 <label>Faculty</label><br></br>
-                <input/>
+                <input  onChange={onInputHandler}/>
             
              
                <div className="btncontainer">
-            <button className="btnsave" onClick={()=>{setToggle(!toggle)}}>Save</button>
+            <button className="btnsave" onClick={onClickHandler}>Save</button>
             <button className="btncancel"  onClick={()=>{setToggle(!toggle)}}>Cancel</button>
                    </div> 
           
---
+++ src/FacultyList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const FacultyList = (props) => {
+    console.log(props.arr)
+  
+    return (
+        <div>
+          app
+        </div>
+    )     
+}
+ 
+export default FacultyList;
